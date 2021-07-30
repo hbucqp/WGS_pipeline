b553123d5f0ba230575d75e9faae2c444841c8b7
Qingpo <hbucqp1991@sina.cn>
2021-07-31 07:19:53 +0800

    add bracken annotaion

--- scripts_example/Species_taxonomy_classification.sh
+++ scripts_example/Species_taxonomy_classification.sh
@@ -14,4 +14,5 @@ do
         base=$(basename $filename .fa)
         # 下行为kraken2运行代码，可根据自己需求进行修改
         kraken2 --use-names --threads 16 --db /disk1/cau/cvmcjp/database/minikraken_8GB_20200312 --report ./result/kraken/${base}.report ./seq/${base}.fa > ./result/kraken/${base}.kraken
+        bracken -d /disk1/cau/cvmcjp/database/minikraken_8GB_20200312 -i ./result/kraken/${base}.report -l S -o ./result/kraken/${base}.txt
 done
