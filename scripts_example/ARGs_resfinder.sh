151901ba776a63d7525f5e5095267ce64bc807d4
Qingpo <hbucqp1991@sina.cn>
2021-09-22 16:50:47 +0800

    newer version AMR analysis with resfinder

--- scripts_example/ARGs_resfinder.sh
+++ scripts_example/ARGs_resfinder.sh
@@ -10,10 +10,37 @@ export PATH=/disk1/cau/cvmcjp/anaconda3/envs/py36/bin:$PATH
 
 # 基于resfinder的耐药基因分析
 
-mkdir -p ./result/resfinder1395
-for filename in ./seq1395/*.fasta
+
+# 点突变支持Species
+# Available species:
+# Campylobacter,
+# Campylobacter jejuni,
+# Campylobacter coli,
+# Enterococcus faecalis,
+# Enterococcus faecium,
+# Escherichia coli,
+# Helicobacter pylori,
+# Klebsiella,
+# Mycobacterium tuberculosis,
+# Neisseria gonorrhoeae,
+# Plasmodium falciparum,
+# Salmonella,
+# Salmonella enterica,
+# Staphylococcus aureus
+# -s "Other" can be used for metagenomic samples or samples with unknown species.
+
+
+
+mkdir -p ./result/resfinder
+for filename in ./seq/*.fa
 do
-        base=$(basename $filename .fasta)
-        mkdir -p ./result/resfinder1395/${base}
-        python3 /disk1/cau/cvmcjp/software/resfinder/resfinder.py -i ./seq1395/${base}.fasta -p /disk1/cau/cvmcjp/database/resfinder_db -o ./result/resfinder1395/${base} -mp blastn -x -t 0.9 -l 0.6
+        base=$(basename $filename .fa)
+        mkdir -p ./result/resfinder/${base}
+        python3 /disk1/cau/cvmcjp/software/resfinder/run_resfinder.py -db_res /disk1/cau/cvmcjp/database/resfinder_db -s Other \
+        --point -db_point /disk1/cau/cvmcjp/database/pointfinder_db \
+        -o ./result/resfinder/${base} -l 0.6 -t 0.9 \
+        --acquired -ifa ./seq/${base}.fa
+        # Deprecated
+        # python3 /disk1/cau/cvmcjp/software/resfinder/resfinder.py -i ./seq1395/${base}.fasta -p /disk1/cau/cvmcjp/database/resfinder_db -o
+        # ./result/resfinder1395/${base} -mp blastn -x -t 0.9 -l 0.6
 done
