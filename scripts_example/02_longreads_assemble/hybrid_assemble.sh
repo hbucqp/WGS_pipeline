ebfbd77da40f45a0319e21879b81b13207b9a2a7
崔箐坡 <cuiqingpo@cuiqingpos-MBP.lan>
2021-12-05 16:08:30 +0800

    hybrid assemble analysis

--- scripts_example/02_longreads_assemble/hybrid_assemble.sh
+++ scripts_example/02_longreads_assemble/hybrid_assemble.sh
@@ -6,10 +6,13 @@
 #$ -pe mpi 16
 
 
+export PATH=/disk1/cau/cvmcjp/miniconda3/envs/nanopore/bin:$PATH
+export PATH=/disk1/public/bcftools-1.9/bin:$PATH
+
 #Hybrid_assembly
 mkdir -p ./result ./result/nanopore_assemble
 for filename in *_1.fq.gz
 do
         base=$(basename $filename _1.fq.gz)
-        unicycler -1 ${base}_1.fq.gz -2 ${base}_2.fq.gz -l ./nanopore/${base}_ps_nanopore.fastq.gz -o ./result/nanopore_assemble/${base} --pilon_path ~/anaconda3/envs/py36/bin/pilon --vcf --bcftools_path ~/anaconda3/envs/py36/bin/bcftools -t 16
+        unicycler -1 ${base}_1.fq.gz -2 ${base}_2.fq.gz -l ./nanopore/${base}_ps_nanopore.fastq.gz -o ./result/nanopore_assemble/${base} --pilon_path ~/miniconda3/envs/nanopore/bin/pilon --vcf --bcftools_path /disk1/public/bcftools-1.9/bin/bcftools -t 16
 done
