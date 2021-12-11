a664869a5cb333c5ca6b6c1e49f6f51841b8ff80
崔箐坡 <cuiqingpo@cuiqingpos-MBP.lan>
2021-12-11 13:30:04 +0800

    delete

--- database/ssuis_vf/ssuis_vf_download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#IFS='\t'
-
-while read a b c
-do
-    echo ">suisvf~~~$a~~~$b~~~$c" >> vf_sequence.fa
-    efetch -db protein -format fasta_cds_na -id $b | grep -v ">" >> vf_sequence.fa
-done < id.txt
